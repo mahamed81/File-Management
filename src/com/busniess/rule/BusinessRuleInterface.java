16117ebcb939102ceb5d8f764d05c12b2e59479b
Mahamed Ahmed <mahamed.ahmed@hcl.com>
2020-12-14 17:32:30 -0600

    first commit

---
+++ src/com/busniess/rule/BusinessRuleInterface.java
@@ -0,0 +1,16 @@
+package com.busniess.rule;
+
+import java.io.IOException;
+
+public interface BusinessRuleInterface {
+	
+	
+	public void addFile() throws IOException;
+	public void createDir();
+	public void deleteFile();
+	public void searchFile();
+	public void showFiles();
+
+
+
+}
